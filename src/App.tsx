bde8c022f09ec894bec0cb3f57612d65a3d35459
santaforse <santaforsell@gmail.com>
2022-12-04 23:24:06 +0200

    stufff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import '../src/assets/css/views/style.css';
+import CategoriesView from './views/CategoriesView';
+import ProductsView from './views/ProductsView';
+import ProductDetailsView from './views/ProductDetailsView';
+import SearchView from './views/SearchView';
+import CompareView from './views/CompareView';
+import ContactsView from './views/ContactsView';
+import HomeView from './views/HomeView';
+import NotFoundView from './views/NotFoundView';
+import WishListView from './views/WishListView';
+import { ProductProvider} from './contexts/ProductContext';
+import { ShoppingCartProvider} from './contexts/ShoppingCartContext';
 
 function App () {
   return (
-    <div>
-     
-    </div>
+    <BrowserRouter>
+          <ShoppingCartProvider>
+          <ProductProvider>
+            <Routes>
+              <Route path="/" element={<HomeView />} />
+              <Route path="/categories" element={<CategoriesView />} />
+              <Route path="/products" element={<ProductsView />} />
+              <Route path="/products/:id" element={<ProductDetailsView />} />
+              <Route path="/contacts" element={<ContactsView />} />
+              <Route path="/search" element={<SearchView />} />
+              <Route path="/compare" element={<CompareView />} />
+              <Route path="/wishlist" element={<WishListView />} />
+              <Route path="*" element={<NotFoundView />} />
+            </Routes>
+          </ProductProvider>
+          </ShoppingCartProvider>
+      </BrowserRouter>
   );
 }
 
